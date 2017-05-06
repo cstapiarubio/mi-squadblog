f40e7134a9ee66a2e31c7ef7ca7356f359b53c65
cstapiarubio <cstapiarubio@gmail.com>
2017-05-05 22:06:19 -0300

    agregue imagenes y boton me gusta

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,6 @@
-function Misquad(nombre,apellido,edad,hobbies,hobbies2,hobbies3){
+
+function Misquad(img,nombre,apellido,edad,hobbies,hobbies2,hobbies3){
+    this.img = img;
     this.nombre = nombre;
     this.apellido = apellido;
     this.edad = edad;
@@ -6,29 +8,45 @@ function Misquad(nombre,apellido,edad,hobbies,hobbies2,hobbies3){
     this.hobbies2 = hobbies2;
     this.hobbies3 = hobbies3;
 }
-var fran = new Misquad("Francisca","Ojeda",28,"Roller derby", "Lettering", "Cocinar Pizza");
-var cote = new Misquad("María José","Pozo",31,"Bailar","Comer","Dormir");
-var mariajose = new Misquad("María José","Rodriguez",32,"Ver series","Bailar","Leer");
-var caro = new Misquad("Carolina","Tapia",27,"Trekking", "leer" , "tejer");
-var camila = new Misquad("Camila","Saez",24,"Origami","Viajar","Comprar cosas en Aliexpress");
-var val = new Misquad("Valentina","Saavedra",26,"Medicina autogestiva","Hacer magias","Hacerse tatuajes");
-var marion = new Misquad("Marion","Castillo",29,"Caminar","Hacer series","Pintar");
-var antonia = new Misquad("Antonia","Cerda",23,"Diseñar", "Ver series", "Lettering");
-var mostrarfran = document.getElementById("fran");
-var mostrarcote = document.getElementById("cote");
-var mostrarmariajose = document.getElementById("mariajose");
-var mostrarcaro = document.getElementById("caro");
-var mostrarcamila = document.getElementById("camila");
-var mostrarval = document.getElementById("val");
-var mostrarmarion = document.getElementById("marion");
-var mostrarantonia = document.getElementById("antonia");
+
+var fran = new Misquad("x","Francisca","Ojeda",28,"Roller derby", "Lettering", "Juegos de mesa");
+var cote = new Misquad("x","María José","Pozo",31,"Bailar","Comer","Dormir");
+var mariajose = new Misquad("x","María José","Rodriguez",32,"Ver series","Bailar","Leer");
+var caro = new Misquad("x","Carolina","Tapia",27,"Trekking", "Leer" , "Tejer");
+var camila = new Misquad("x","Camila","Saez",24,"Origami","Viajar","Comprar cosas en Aliexpress");
+var val = new Misquad("x","Valentina","Saavedra",26,"Medicina autogestiva","Hacer magias","Hacerse tatuajes");
+var marion = new Misquad("x","Marion","Castillo",29,"Caminar","Hacer series","Pintar");
+var antonia = new Misquad("x","Antonia","Cerda",23,"Diseñar", "Ver series", "Lettering");
+
+
 var arrusuario = [];
+
 arrusuario.push(fran,cote,mariajose,caro,camila,val,marion,antonia);
+
 arrusuario.forEach(function(ele){
-    document.write("<div>"+"<b>Nombre: </b>"+ele.nombre+ " " +
-     ele.apellido+ "<br>" + "<b>Edad : </b>"+  ele.edad+ "<br>"+ "<b>Hobbies : </b> <i>" + "<ul>" + "<li>" + ele.hobbies+ "</li>"+"<li>" +ele.hobbies2+ 
-     "</li>"+ "<li>"+ ele.hobbies3+ "</li></ul></i><br></b></div>")
+    var divlistasquad = document.getElementById("listasquad");
+    var usuario = document.createElement("div");
+    divlistasquad.appendChild(usuario);
+    //var dentroDiv = document.createTextNode(info);
+    
+
+    var info = (ele.img+ "<br>" +"<b>Nombre: </b>"+ele.nombre+ " " +
+         ele.apellido+ "<br>" + "<b>Edad : </b>"+  ele.edad+ "<br>"+ "<b>Hobbies : </b> <i>" + "<ul>" + "<li>" + ele.hobbies+ "</li>"+"<li>" +ele.hobbies2+ 
+         "</li>"+ "<li>"+ ele.hobbies3 + "</li></ul></i><br></b>"); 
+    divlistasquad.innerHTML += info;
+
 })
+
+
+function agregarImagen()
+{
+    var objDiv = document.getElementById("imagen");
+  var newImage = document.createElement("img");
+  newImage.src = "http://quebolu.com/uploads/meme1419396805gen.png";
+  objDiv.appendChild(newImage);
+}
+
+
 var numero=0;
 function sumar (){ 
 numero++;
@@ -37,4 +55,3 @@ return numero;
 function hacerClick(){
     document.getElementById("caja").innerHTML = sumar();
 }
-
