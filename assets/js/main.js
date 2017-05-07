45d09a0ffeee03897e80727dc487bd6485b334d6
cstapiarubio <cstapiarubio@gmail.com>
2017-05-07 01:27:59 -0300

    sigo probando codigos para ingresar los comentarios, añadi fotos de perfil de compañeras

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,52 +1,54 @@
-var arrusuario = [];
 
-function Misquad(img,nombre,apellido,edad,hobbies,hobbies2,hobbies3){
-    this.img = img;
-    this.nombre = nombre;
-    this.apellido = apellido;
-    this.edad = edad;
-    this.hobbies = hobbies;
-    this.hobbies2 = hobbies2;
-    this.hobbies3 = hobbies3;
-    this.caja = "<input type='text' id='boton" + this.id + "'>"; //aca se ingresa el comentario
-    this.boton = "<button id='" + this.id + "' onclick='" + /*nuevoComentario.likes() +*/ "'>Dejar Comentario</button>"; //boton dejar comentario
+//constructor del squad
+
+function imprimir(){
+  document.getElementById("contenedor").innerHTML =comentario();
 }
 
+function comentario(){
+    // tomo texto de textarea
+    var coment = document.getElementById("ingresoComent").value;
+    //se limpia textarea
+    document.getElementById("ingresoComent").value = "";
+    // creo elemento div 
+    var nuevosComents = document.createElement("div");
+    //contenedor donde irán las tareas
+    var cont = document.getElementById("contenedor");
+  }
 
-var fran = new Misquad("x","Francisca","Ojeda",28,"Roller derby", "Lettering", "Juegos de mesa");
-var cote = new Misquad("x","María José","Pozo",31,"Bailar","Comer","Dormir");
-var mariajose = new Misquad("x","María José","Rodriguez",32,"Ver series","Bailar","Leer");
-var caro = new Misquad("x","Carolina","Tapia",27,"Trekking", "Leer" , "Tejer");
-var camila = new Misquad("x","Camila","Saez",24,"Origami","Viajar","Comprar cosas en Aliexpress");
-var val = new Misquad("x","Valentina","Saavedra",26,"Medicina autogestiva","Hacer magias","Hacerse tatuajes");
-var marion = new Misquad("x","Marion","Castillo",29,"Caminar","Hacer series","Pintar");
-var antonia = new Misquad("x","Antonia","Cerda",23,"Diseñar", "Ver series", "Lettering");
-
-
-arrusuario.push(fran,cote,mariajose,caro,camila,val,marion,antonia);
-
-arrusuario.forEach(function(ele){
-    var divlistasquad = document.getElementById("listasquad");
-    var usuario = document.createElement("div");
-    divlistasquad.appendChild(usuario);
-    //var dentroDiv = document.createTextNode(info);
-    
-
-    var info = (ele.img+ "<br>" +"<b>Nombre: </b>"+ele.nombre+ " " +
-         ele.apellido+ "<br>" + "<b>Edad : </b>"+  ele.edad+ "<br>"+ "<b>Hobbies : </b> <i>" + "<ul>" + "<li>" + ele.hobbies+ "</li>"+"<li>" +ele.hobbies2+ 
-         "</li>"+ "<li>"+ ele.hobbies3 + "</li></ul></i><br></b>" + ele.caja + ele.boton); 
-    divlistasquad.innerHTML += info;
-
-})
-
-
-function agregarImagen()
-{
-    var objDiv = document.getElementById("imagen");
-  var newImage = document.createElement("img");
-  newImage.src = "http://quebolu.com/uploads/meme1419396805gen.png";
-  objDiv.appendChild(newImage);
+function MiembroSquad(nombre,apellido,edad,id,hobbie1,hobbie2,hobbie3){
+  this.nombre = nombre;
+  this.apellido = apellido;
+  this.edad = edad;
+  this.id = id;
+  this.hobbie1 = hobbie1;
+  this.hobbie2 = hobbie2;
+  this.hobbie3 = hobbie3;
+  this.foto = "<img src='assets/imgs/avataressquad-0" + this.id + "_360.png'>";
+  this.caja = "<input type='text' id='ingresoComent" + this.id + "'>"; //aca se ingresa el comentario
+  this.contenedor = function comentar() { document.getElementByI("caja").innerHTML =imprimir();}//"<input type='text' id='contenedor" + this.id + "'>";//aca se guardan los comentarios
+  this.boton = "<button id='boton' 'onclick= imprimir()"+ this.id + "'>Dejar Comentario</button>"; /*nuevoComentario.likes() +*/  //boton dejar comentario
 }
+
+var lista = new Array(8);
+lista[0] = new MiembroSquad("Camila","Saez",24,"1","Ver pinterest","Viajar","Comprar cosas en Aliexpress");
+lista[1] = new MiembroSquad("Antonia","Cerda",23,"2","Diseñar","Ver series","Lettering");
+lista[2] = new MiembroSquad("Carolina","Tapia",27,"3","Trekking","Leer","Tejer");
+lista[3] = new MiembroSquad("María José","Rodriguez",32,"4","Ver series","Bailar","Leer");
+lista[4] = new MiembroSquad("María José","Pozo",31,"5","Bailar","Comer","Dormir");
+lista[5] = new MiembroSquad("Francisca","Ojeda",28,"6","Rollerderby","Lettering","Juegos de mesa");
+lista[6] = new MiembroSquad("Valentina","Saavedra",26,"7","Medicina autogestiva","Hacer magia","Hacerse tatuajes");
+lista[7] = new MiembroSquad("Marion","Castillo",29,"8","Caminar","Pintar","Fotografiar");
+
+lista.forEach(function(element){
+  var miembros = document.getElementById("miembro" + lista.indexOf(element).toString());
+  miembros.innerHTML = (element.foto +"<br><b>Nombre: </b>" + element.nombre + "<br><b>Apellido: </b>" + element.apellido + 
+  "<br><b>Edad: </b>" + element.edad + "<br><b>Hobbies: </b>" + "<ul>" + "<li>" + element.hobbie1 + "</li>" + 
+  "<li>" + element.hobbie2 + "</li>" + "<li>" + element.hobbie3 + "</li>" + "</ul>" + element.caja + element.contenedor +
+   "<br>" + element.boton);
+});
+
+MiembroSquad.contenedor();
 
 var numero=0;
 function sumar (){ 
@@ -54,52 +56,45 @@ numero++;
 return numero;
 }
 
+function hacerClick(){
+       document.getElementById("caja").innerHTML = sumar();
+    }
+
+
+
 //aca no he hecho nada
-function Comentario(mensaje,comentario,likes){
+/*function Comentario(mensaje,comentario,likes){
     this.mensaje = mensaje;
     this.comentario = comentario;
-    this.likes = function hacerClick(){
-       document.getElementById("mensaje").innerHTML = sumar();
-    };
+    this.likes = 
     //this.botonLike = "<button onclick=hacerClick() + this.likes() + "'></button>;" 
 }
 
+
+
+//aca no he hecho nada
+function Comentario(id_miembro,comentario,likes){
+  this.id_miembro = id_miembro;
+  this.comentario = comentario;
+  this.likes = function (){
+    var contador = 0;
+    return (contador + 1);
+  };
+  this.botonLike = "<button onclick='" + this.likes() + "'></button>";
+}
+
+
+    
 //esto no me funciona
-var miembro = document.getElementById("mensaje");
+var miembro = document.getElementById("miembro0");
+var member = document.getElementById("m0");
 var coment = lista[0].caja.value;
 var nuevoComentario = new Comentario(miembro,coment);
-miembro.innerHTML = nuevoComentario.coment;
+miembro.innerHTML = nuevoComentario.coment;*/
 
 
-var miUl = document.getElementById("mensaje");
-var liAux, textAux;
 
-mensaje.forEach(renderTask);
 
-function renderTask(task){
-    var liAux = document.createElement("li");
-    var textAux = document.createTextNode(task.title + "  ");
-    var enlace =  document.createElement("a");
-    var textEdit = document.createTextNode("Editar");
-    liAux.appendChild(textAux); 
-  miUl.appendChild(liAux);
-  enlace.appendChild(textEdit);
-  enlace.setAttribute("onclick", "hacerClick(this, " + task.id +")");
-  liAux.appendChild(enlace);
-}
 
-function editTask(enlace, idTask){
-    console.log(enlace);
-    console.log(idTask);
-    var nuevoTexto = prompt("Ingrese el nuevo texto");
-    var mensaje = mensaje.filter(function(el){
-        return el.id == idTask;
-    });
-    tarea[0].title = nuevoTexto;
-
-    var miLi = enlace.parentNode;
-    miLi.innerHTML = nuevoTexto;
-    miLi.appendChild(enlace);
-}
 
 
